471f4c3f9dad4acd75cb0d539f2905c963f7e88c
Nebi <mam10122011@yahoo.com>
2021-04-03 15:44:54 +0300

    database connected

---
+++ app.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import connectMongo from "./db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+(async () => {
+  try {
+    const connect = await connectMongo();
+    if (connect) {
+      console.log("connected succesfully");
+    }
+
+    const server = new ApolloServer({
+      typeDefs: schemas,
+      resolvers,
+    });
+
+    const app = express();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: 3000 }, () =>
+      console.log(
+        `🚀 Server ready at http://localhost:3000${server.graphqlPath}`
+      )
+    );
+  } catch (e) {
+    console.log("server error: " + e.message);
+  }
+})();
