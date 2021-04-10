157fcc57d0ec510e61e8f7641e87f8a52c6b9162
Nebi <mam10122011@yahoo.com>
2021-04-10 21:02:37 +0300

    security done

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import dotenv from "dotenv";
 import schemas from "./schemas/index.js";
 import resolvers from "./resolvers/index.js";
 import { checkAuth } from "./passport/authenticate.js";
+import localhost from "./security/localhost.js";
+import production from "./security/production.js";
+import helmet from "helmet";
 
 dotenv.config();
 
@@ -32,14 +35,21 @@ dotenv.config();
     });
 
     const app = express();
+    app.use(helmet.hidePoweredBy());
 
     server.applyMiddleware({ app });
+    process.env.NODE_ENV = process.env.NODE_ENV || "development";
+    if (process.env.NODE_ENV === "production") {
+      production(app, 3000);
+    } else {
+      localhost(app, 8000, 3000);
+    }
 
-    app.listen({ port: 3001 }, () =>
+    /*app.listen({ port: 3001 }, () =>
       console.log(
         `🚀 Server ready at http://localhost:3001${server.graphqlPath}`
       )
-    );
+    );*/
   } catch (e) {
     console.log("server error: " + e.message);
   }
